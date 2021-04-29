9823c3ad9d2b73d26a5a86871c55d4186fb70ef2
Yoon-Jo Hwang <mymelody112266@gmail.com>
2021-04-29 21:48:25 +0900

    Initial Commit

---
+++ GitXcode2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitXcode2
+//
+//  Created by Release on 2021/04/29.
+//  Copyright © 2021 Hwang-Yoon-Jo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
